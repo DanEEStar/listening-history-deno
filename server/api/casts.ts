5b4c459a342708455c025b2e95d6228fa6cd04e3
Daniel Egger <daniel.egger@gmail.com>
2023-11-23 14:07:56 +0100

    Add some logging

--- server/api/casts.ts
+++ server/api/casts.ts
@@ -1,6 +1,10 @@
 import { lastPocketCastsEpisodeDb } from "~/server/pocketCasts.ts";
+import consola from "consola";
+import { env } from "node:process";
 
 export default defineEventHandler(async (event) => {
+  consola.log("start /api/casts endpoint");
+  consola.log("SPOTIFY_CLIENT_ID", env.SPOTIFY_CLIENT_ID);
   const lastPocketCastsEpisode = await lastPocketCastsEpisodeDb();
   return lastPocketCastsEpisode;
 });
