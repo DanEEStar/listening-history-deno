8749e925ebc981fb566e96169787d3f9107f8f50
Daniel Egger <daniel.egger@gmail.com>
2024-11-13 15:44:37 +0100

    Add nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxt/ui"],
+  extends: ["@nuxt/ui-pro"],
+});
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <ULandingHero description="Nuxt UI Pro is a collection of premium Vue components built on top of Nuxt UI to create beautiful & responsive Nuxt applications in minutes.">
+      <template #title>
+        The <span class="text-primary block lg:inline-block">Building Blocks</span> for Modern Web apps
+      </template>
+    </ULandingHero>
+
+    <ULandingSection title="The freedom to build anything" align="left" />
+
+    <ULandingSection title="The flexibility to control your data" align="right" />
+  </div>
+</template>
