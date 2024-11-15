7a4ef653cba72f247cf2f786bebc78dc0a51fc37
Daniel Egger <daniel.egger@gmail.com>
2024-11-15 11:48:20 +0100

    Add workaround for buffer import in deno deploy

--- server/api/spotify/play.post.ts
+++ server/api/spotify/play.post.ts
@@ -1,7 +1,10 @@
 import { playTrack } from "~/server/services/spotify.ts";
 
+
 export default defineEventHandler(async (event) => {
+  console.log("spotify play (first line)");
   const body = await readBody(event);
+  console.log("spotify play", body);
   await playTrack(body.album_uri, body.track_number - 1, body.device_id ?? null);
   return { status: "success" };
 });
