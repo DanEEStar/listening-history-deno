4604c0ba8fbb4fbb5ef7b1d717e35740bc152dae
Daniel Egger <daniel.egger@gmail.com>
2026-04-13 09:05:23 +0200

    fix in backup script

--- scripts/create_backup.sh
+++ scripts/create_backup.sh
@@ -9,4 +9,4 @@ pg_dump $SUPABASE_DATABASE_URL -Fc --no-acl --no-owner > /Users/daniel/Dropbox/b
 cp /Users/daniel/Dropbox/backup/listening-history/supabase.dump "/Users/daniel/Dropbox/backup/listening-history/archive/supabase_$(date +%Y-%m-%d).dump"
 
 # rotate postgres-archives
-uvx rotate-backups --daily=7 --weekly=7 --monthly=25 --yearly=5 /Users/daniel/Dropbox/backup/listening-history/archive
+uvx --python 3.12 rotate-backups --daily=7 --weekly=7 --monthly=25 --yearly=5 /Users/daniel/Dropbox/backup/listening-history/archive
